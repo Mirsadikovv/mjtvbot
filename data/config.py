03c21e6eaf037058f770a7c38166dc1fac18a250
Mirodil <mirsadikovmirodil5610056@gmail.com>
2023-09-12 11:20:47 +0500

    first commit

--- data/config.py
+++ data/config.py
@@ -8,3 +8,9 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 IP = env.str("ip")  # Xosting ip manzili
+
+
+DP_USER = env.str("DP_USER")
+DP_PASS = env.str("DP_PASS")
+DP_NAME = env.str("DP_NAME")
+DP_HOST = env.str("DP_HOST")
